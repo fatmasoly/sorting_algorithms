f5a6ef901188506deca8f2cbc0505df52f99ae7e
root <root@DESKTOP-CTTDNQL.localdomain>
2024-01-20 13:52:53 +0300

    gg

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,63 +1,48 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - sorts a doubly linked list of integers in
- * ascending order using the Insertion sort algorithm
- * @list: double pointer to head of list
+ * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
+ * @h: A pointer to the head of the doubly-linked list.
+ * @n1: A pointer to the first node to swap.
+ * @n2: The second node to swap.
  */
-void insertion_sort_list(listint_t **list)
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 {
-listint_t *sorted = NULL;
-listint_t *current = *list;
-listint_t *next;
-
-while (current != NULL)
-{
-next = current->next;
-current->next = current->prev = NULL;
-sorted = sortedInsert(sorted, current);
-current = next;
-print_list(*list);
-}
-
-*list = sorted;
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		*h = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
 }
 
 /**
- * sortedInsert - inserts a node in a sorted way
- * in a sorted doubly linked list
- * @head_ref: head of the sorted list
- * @newNode: node to be inserted
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ *                       using the insertion sort algorithm.
+ * @list: A pointer to the head of a doubly-linked list of integers.
  *
- * Return: updated head of the sorted list
+ * Description: Prints the list after each swap.
  */
-listint_t *sortedInsert(listint_t *head_ref, listint_t *newNode)
+void insertion_sort_list(listint_t **list)
 {
-listint_t *current;
+	listint_t *iter, *insert, *tmp;
 
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-if (head_ref == NULL || head_ref->n >= newNode->n)
-{
-newNode->next = head_ref;
-newNode->prev = NULL;
-if (head_ref != NULL)
-head_ref->prev = newNode;
-head_ref = newNode;
-}
-else
-{
-
-current = head_ref;
-while (current->next != NULL && current->next->n < newNode->n)
-current = current->next;
-
-
-newNode->next = current->next;
-newNode->prev = current;
-if (current->next != NULL)
-current->next->prev = newNode;
-current->next = newNode;
-}
-
-return (head_ref);
+	for (iter = (*list)->next; iter != NULL; iter = tmp)
+	{
+		tmp = iter->next;
+		insert = iter->prev;
+		while (insert != NULL && iter->n < insert->n)
+		{
+			swap_nodes(list, &insert, iter);
+			print_list((const listint_t *)*list);
+		}
+	}
 }
